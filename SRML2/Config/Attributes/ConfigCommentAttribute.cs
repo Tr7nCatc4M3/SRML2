c681b88560246901e8419de40a13555b6ce5699b
Tr7nCatc4M3 <114768946+Tr7nCatc4M3@users.noreply.github.com>
2022-10-19 21:50:40 +0000

    Add files via upload

---
+++ SRML2/Config/Attributes/ConfigCommentAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SRML2.Config.Attributes
+{
+	public class ConfigCommentAttribute
+	{
+		public string comment;
+
+		public ConfigCommentAttribute(string comment)
+		{
+			this.comment = comment;
+		}
+	}
+}
---
+++ SRML2/Config/Attributes/ConfigFIleAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SRML2.Config.Attributes
+{
+	public class ConfigFIleAttribute
+	{
+		internal string fileName;
+		internal string defaultSection;
+
+		public ConfigFIleAttribute(string fileName, string defaultSection)		{
+			this.fileName = fileName;
+			this.defaultSection = defaultSection;
+		}
+	}
+}
